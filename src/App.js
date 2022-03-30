c57a425a81e203b4759df33a814b396982b24c31
kr <hoylfree@gmail.com>
2022-03-30 16:15:14 +0900

    react-redux & tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import "./App.css";
+import "tailwindcss/tailwind.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Hi</h2>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
