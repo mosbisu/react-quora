53d2ae0f5125bf12aa10f0e5eadb3966dc9aa438
kr <hoylfree@gmail.com>
2022-03-30 23:18:10 +0900

    redux setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Quora from "./components/Quora";
 import "tailwindcss/tailwind.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./components/Login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="m-0 h-auto w-auto bg-slate-100">
-      <Quora />
+      {user ? <Quora /> : <Login />}
     </div>
   );
 }
