e34b8774788e9b93d7069370d1a97f8edd923c96
kr <hoylfree@gmail.com>
2022-04-01 12:35:14 +0900

    Modal

--- src/components/FeedContents.js
+++ src/components/FeedContents.js
@@ -7,11 +7,15 @@ import {
   RepeatOneOutlined,
   ShareOutlined,
 } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
+// Modal.setAppElement("#root");
 
 function FeedContents({ key, Id, image, question, timestamp, quoraUser }) {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="flex flex-col p-[10px] border border-green-600 bg-white rounded-[5px] cursor-pointer mt-[10px] hover:boder-2 hover:border-[olive]">
+    <div className="flex flex-col p-[10px] border border-green-600 bg-white rounded-[5px] cursor-pointer mt-[10px] hover:border-2 hover:border-[olive]">
       <div className="flex items-center">
         <Avatar src={quoraUser.photo} />
         <h5 className="text-green-600 ml-[10px] cursor-pointer text-[13px] hover:underline">
@@ -24,9 +28,70 @@ function FeedContents({ key, Id, image, question, timestamp, quoraUser }) {
       <div className="flex flex-col">
         <div className="my-[10px] font-bold cursor-pointer flex">
           <p className="hover:underline">{question}</p>
-          <button className="ml-auto outline-none border-none bg-[lightgoldenrodyellow] text-green-600 rounded-[10px] p-[10px] hover:scale-110">
+          <button
+            className="ml-auto outline-none border-none bg-[lightgoldenrodyellow] text-green-600 rounded-[10px] p-[10px] hover:scale-110"
+            onClick={() => setOpenModal(true)}
+          >
             답변하기
           </button>
+          <Modal
+            ariaHideApp={false}
+            style={{
+              overlay: {
+                width: 700,
+                height: 600,
+                backgroundColor: "rgba(0,0,0,0.8)",
+                zIndex: 1000,
+                top: "50%",
+                left: "50%",
+                marginTop: "-300px",
+                marginLeft: "-350px",
+              },
+            }}
+            isOpen={openModal}
+            onRequestClose={() => {
+              setOpenModal(false);
+            }}
+            shouldCloseOnOverlayClick={false}
+          >
+            <div className="flex flex-col items-center">
+              <h1 className="text-[#8f1f1b] text-3xl font-semibold mb-[10px]">
+                {question}
+              </h1>
+              <p className="text-[gray] text-sm">
+                <span className="text-black text-sm">
+                  {quoraUser.displayName
+                    ? quoraUser.displayName
+                    : quoraUser.email}
+                </span>
+                로부터의 질문
+                <span>{new Date(timestamp?.toDate()).toLocaleString()}</span>에
+                작성됨
+              </p>
+            </div>
+            <div className="flex flex-1 pt-[20px]">
+              <textarea
+                className="w-full h-[300px] p-[5px] text-[15px] text-black border"
+                type="text"
+                placeholder="답변을 작성해주세요"
+              />
+            </div>
+
+            <div className="flex items-center mr-[-20px] mt-0">
+              <button
+                className="border-none outline-none mt-[5px] mr-[10px] text-white bg-[blue] font-bold p-[10px] rounded-[33px] cursor-pointer"
+                type="submit"
+              >
+                답변달기
+              </button>
+              <button
+                className="border-none outline-none mt-[5px] ml-auto mr-[10px] text-[gray] font-medium p-[10px] rounded-[33px] cursor-pointer"
+                onClick={() => setOpenModal(false)}
+              >
+                닫기
+              </button>
+            </div>
+          </Modal>
         </div>
         <div>
           <p>답변입니다</p>
